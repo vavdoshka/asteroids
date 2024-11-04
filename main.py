04c2e2ef5406113d6a54573aa7c226140757b8f1
Vladimir Avdoshka <vova.avdoshka@gmail.com>
2024-11-04 13:40:47 +0100

    built player logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -7,12 +8,34 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    updatable.add(player)
+    drawable.add(player)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color="#000000")
+        
+        screen.fill(color="black")
+
+        for upd in updatable:
+            upd.update(dt)
+        for drw in drawable:
+            drw.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+
+        
 
 
 if __name__ == '__main__':
